79a257f8069d312f22e263350a1979d28d6044f4
oskarmast <oskarmast9436@gmail.com>
2024-09-30 18:51:43 +0200

    add: setting tab

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu } = require('electron');
 const path = require('path');
+const fs = require('fs');
 const spawn = require('child_process').spawn;
+const url = require('url');
 
 let mainWindow;
+const loginFilePath = path.join(app.getPath('userData'), 'login.json');
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -14,8 +17,74 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadFile('login.html');
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'login.html'),
+    protocol: 'file:',
+    slashes: true,
+  }));
 }
+
+const template = [
+  {
+    label: 'File',
+    submenu: [
+      { role: 'quit' }
+    ]
+  },
+  {
+    label: 'Settings',
+    click() {
+      mainWindow.webContents.loadURL(url.format({
+        pathname: path.join(__dirname, 'settings.html'),
+        protocol: 'file:',
+        slashes: true,
+      }));
+    }
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload',
+        role: 'reload'
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Cmd+Alt+I' : 'Ctrl+Shift+I',
+        click() {
+          mainWindow.webContents.toggleDevTools();
+        }
+      }
+    ]
+  },
+  {
+    label: 'Help',
+    submenu: [
+      { role: 'about' }
+    ]
+  }
+];
+
+
+// Create menu from template
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
+
+// Read the login.json file and send data to the login page
+ipcMain.handle('get-login-info', async () => {
+  try {
+    const data = fs.readFileSync(loginFilePath, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    return {}; // Return empty if file not found or error
+  }
+});
+
+// Save the login information to login.json
+ipcMain.on('save-login-info', (event, loginInfo) => {
+  fs.writeFileSync(loginFilePath, JSON.stringify(loginInfo, null, 2));
+  console.log('Login info saved to', loginFilePath);
+});
 
 const squirrelEvent = process.argv[1];
 
